0a6ec916e160ac30a4663409da43133db3146c16
louisLSCHVN <louis.leschevin@gmail.com>
2024-05-13 15:52:05 +0200

    lemon

---
+++ pages/account/index.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <p>avatar</p>
+    <p>location</p>
+    <p>first name</p>
+    <p>last name</p>
+    <p>bio</p>
+    <p>website</p>
+    <p>pronous</p>
+
+</template>
--- pages/book/[id].vue
+++ pages/book/[id].vue
@@ -1,19 +1,27 @@
 <template>
   <ReviewAdd :book_id="id" />
-  <pre><code>{{ book }}</code></pre>
   <article
-    v-if="state.reviews"
+    v-if="state.reviews && state.reviews.length > 0"
     v-for="(review, index) in state.reviews"
     :key="index"
   >
     <h3>{{ review.content }}</h3>
     <p>Rating: {{ review.rating }}</p>
     <p>Spoiler: {{ review.spoiler }}</p>
+    <p
+      v-if="user.data && review.user_id === user.data.user_id"
+      @click="deleteReview(review.review_id)"
+    >
+      DELETE
+    </p>
   </article>
+  <pre><code>{{ book }}</code></pre>
 </template>
 <script setup>
 const route = useRoute();
 const id = ref(route.params.id);
+
+const user = useUsers();
 
 const book = ref({});
 const state = reactive({
@@ -23,25 +31,45 @@ const state = reactive({
 });
 
 const getBookById = async () => {
-  const { data: res } = await useFetch("/api/book/single/" + id.value);
-  console.log(res.value);
-  if (res.value.status !== 200) {
-    state.message = res.value.message;
+  const res = await $fetch("/api/book/single/" + id.value);
+  console.log(res);
+  if (res.status !== 200) {
+    state.message = res.message;
     return;
   }
-  state.message = res.value.message;
-  book.value = res.value.data;
+  state.message = res.message;
+  if (Array.isArray(res.data.items)) {
+    book.value = res.data.items.find((bk) => {
+      return bk.id === id.value;
+    });
+  } else {
+    book.value = res.data.items;
+  }
 };
 
 const getReviews = async () => {
-  const { data: res } = await useFetch("/api/review/" + id.value);
-  console.log(res.value);
-  if (res.value.status !== 200 || !res.value.data) {
-    state.message = res.value.message;
+  const res = await $fetch("/api/review/book/" + id.value);
+  if (res.status !== 200 || !res.data) {
+    state.message = res.message;
     return;
   }
-  state.message = res.value.message;
-  state.reviews = res.value.data;
+  state.message = res.message;
+  state.reviews = res.data;
+};
+
+const deleteReview = async (id) => {
+  const res = await $fetch("/api/review/" + id, {
+    method: "DELETE",
+    credentials: "include",
+  });
+  console.log(res);
+  await getReviews();
+
+  if (res.status !== 200) {
+    state.message = res.message;
+    return;
+  }
+  return res;
 };
 
 if (id.value) {
